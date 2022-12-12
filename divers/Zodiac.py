b1f0b6c9da6e8ca615ba788fb19498818fd2593e
Behrouz <behrouz.safari@gmail.com>
2022-12-12 19:26:36 +0100

    Add files via upload

---
+++ divers/Zodiac.py
@@ -0,0 +1,19 @@
+dc = {
+    'ARIES': '\u2648',
+    'TAURUS': '\u2649',
+    'GEMINI': '\u264A',
+    'CANCER': '\u264B',
+    'LEO': '\u264C',
+    'VIRGO': '\u264D',
+    'LIBRA': '\u264E',
+    'SCORPIUS': '\u264F',
+    'SAGITTARIUS': '\u2650',
+    'CAPRICORN': '\u2651',
+    'AQUARIUS': '\u2652',
+    'PISCES': '\u2653',
+    'OPHIUCHUS': '\u26CE'
+    }
+    
+
+for k,v in dc.items():
+    print(k, v)
