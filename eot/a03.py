877d81df4279d59383d1e7b2e122d7a02afa3605
Behrouz <behrouz.safari@gmail.com>
2022-12-25 21:31:34 +0100

    Update a03.py

--- eot/a03.py
+++ eot/a03.py
@@ -10,9 +10,11 @@ x = df['time'].values[200:210]
 y = df['2022'].values[200:210]
 
 f = interpolate.interp1d(x, y)
+ff = interpolate.interp1d(x, y, kind='cubic') #linear, quadratic, cubic
 
-y_ = f(x)
 
-plt.plot(x, y, 'o', x, y_, '-')
+xnew = np.linspace(x[0], x[-1], 100)
+
+plt.plot(x, y, 'o', xnew, f(xnew), '-')
+plt.plot(x, y, 'o', xnew, ff(xnew), '-')
 plt.show()
-
